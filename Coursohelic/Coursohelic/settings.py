39d692fc0d995af63288b183835aaafb1291f9b0
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-09-14 01:46:08 +0600

    Landing Page

--- Coursohelic/Coursohelic/settings.py
+++ Coursohelic/Coursohelic/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'BackEnd',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Coursohelic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Template'),],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
