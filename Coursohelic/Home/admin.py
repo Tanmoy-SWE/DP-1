91c62bb6401b24a52dd449512a879a914b6b1247
mamanyusufkhan <mamanyusufkhan@gmail.com>
2022-09-21 11:06:29 +0600

    Made more models
    Created session models

--- Coursohelic/Home/admin.py
+++ Coursohelic/Home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Admin, Course_Coordinator, Course_Instructor, Semester, Course_Session, Department, Course, Program_Outcome
 
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(Course_Coordinator)
+admin.site.register(Course_Instructor)
+admin.site.register(Semester)
+admin.site.register(Course_Session)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Program_Outcome)
--- Coursohelic/Home/models.py
+++ Coursohelic/Home/models.py
@@ -35,3 +35,36 @@ class Course_Instructor(models.Model):
     number = models.CharField(max_length=12)
     username = models.CharField(max_length=60)
     password = hashlib.sha256(str(models.CharField(max_length=60)).encode('utf-8')).hexdigest()
+
+class Department(models.Model):
+    d_id = models.CharField(max_length = 20, primary_key=True)
+    name = models.CharField(max_length=200)
+    established_on = models.DateTimeField(auto_now_add=True)
+    opened_by =  models.ForeignKey(Admin, on_delete=models.SET_NULL, blank=True, null=True)
+
+class Semester(models.Model):
+    id = models.CharField(max_length = 20, primary_key=True)
+    semester_name = models.CharField(max_length=200)
+    semester_length = models.CharField(max_length=100)
+    year = models.CharField(max_length=10)
+
+class Course(models.Model):
+    c_id = models.CharField(max_length = 20, primary_key=True)
+    course_code = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    credit = models.DecimalField(max_digits=10, decimal_places=2)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, blank=True, null=True)
+
+class Course_Session(models.Model):
+    csid = models.CharField(max_length = 20, primary_key=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, blank=True, null=True)
+    coordinator = models.ForeignKey(Course_Coordinator, on_delete=models.SET_NULL, blank=True, null=True)
+    instructor = models.ForeignKey(Course_Instructor, on_delete=models.SET_NULL, blank=True, null=True)
+    section = models.CharField(max_length=20)
+    total_CO_marks = models.BigIntegerField()
+
+
+class Program_Outcome(models.Model):
+    p_id = models.CharField(max_length = 20, primary_key=True)
+    description = models.CharField(max_length=1000)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, blank=True, null=True)
