b9fceb9294c9e8c3dca7119552c6c4361e58ddfd
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-10-11 11:42:03 +0600

    Apps Updated

--- Coursohelic/Coursohelic/settings.py
+++ Coursohelic/Coursohelic/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Home',
+    'Coordinator',
+    'Instructor',
+    'InstitutionAdmin',
 ]
 
 MIDDLEWARE = [
