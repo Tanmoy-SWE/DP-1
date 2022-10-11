101eb2d7bbb1c3818aada022a151998034b5ce53
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-10-11 21:34:47 +0600

    Coordinator URL Mapped

--- Coursohelic/Home/urls.py
+++ Coursohelic/Home/urls.py
@@ -6,5 +6,4 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home, name='home'),
     path('courseInstructorForm/', views.cour_ins_form, name='courseInstructorForm'),
-    path('coordinatorForm/', views.coordinator, name='coordinatorForm'),
 ]
--- Coursohelic/Home/views.py
+++ Coursohelic/Home/views.py
@@ -6,6 +6,3 @@ def home(request):
 
 def cour_ins_form(request):
     return render(request , 'CourseInstructorForm.html')
-
-def coordinator(request):
-    return render(request , 'CoordinatorForm.html')
