ce4e8493eb7a94c11697e727d286b96968c36e7b
mamanyusufkhan <mamanyusufkhan@gmail.com>
2022-10-12 01:48:33 +0600

    Altered Some Models

--- Coursohelic/Home/models.py
+++ Coursohelic/Home/models.py
@@ -6,11 +6,9 @@ import hashlib
 
 # Create your models here.
 class institutionAdmin(models.Model):
-    id = models.CharField(max_length = 20, primary_key=True)
     name = models.CharField(max_length=200)
     insititution = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
-    number = models.CharField(max_length=12)
     username = models.CharField(max_length=60)
     password = hashlib.sha256(str(models.CharField(max_length=60)).encode('utf-8')).hexdigest()
     
@@ -18,21 +16,17 @@ class institutionAdmin(models.Model):
 
 
 class Course_Coordinator(models.Model):
-    id = models.CharField(max_length = 20, primary_key=True)
     name = models.CharField(max_length=200)
     insititution = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
-    number = models.CharField(max_length=12)
     username = models.CharField(max_length=60)
     password = hashlib.sha256(str(models.CharField(max_length=60)).encode('utf-8')).hexdigest()
 
 
 class Course_Instructor(models.Model):
-    id = models.CharField(max_length = 20, primary_key=True)
     name = models.CharField(max_length=200)
     insititution = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
-    number = models.CharField(max_length=12)
     username = models.CharField(max_length=60)
     password = hashlib.sha256(str(models.CharField(max_length=60)).encode('utf-8')).hexdigest()
 
