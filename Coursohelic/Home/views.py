15f487059b8e6e8cedc834bcc15b61448d650a22
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-10-15 01:04:40 +0600

    Pages Integrated

--- Coursohelic/Home/views.py
+++ Coursohelic/Home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
-    return render(request , 'Home.html')
+    return render(request , 'LoginPage.html')
 
 
