25abc3541a2091c91459669f253b19de657312af
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-10-15 20:16:49 +0600

    Linked Hard Coded List Pages in Admin

--- Coursohelic/InstitutionAdmin/urls.py
+++ Coursohelic/InstitutionAdmin/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('ProgramCoordinatorList/', views.CoordinatorList, name='CoordinatorList'),
+    path('ProgramList/', views.ProgramList, name='ProgramList'),
     # path('courseInstructorForm/', views.cour_ins_form, name='courseInstructorForm'),
     # path('coordinatorForm/', views.coordinator, name='coordinatorForm'),
 ]
--- Coursohelic/InstitutionAdmin/views.py
+++ Coursohelic/InstitutionAdmin/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request , 'AdminHome.html')
+
+def CoordinatorList(request):
+    return render(request, 'ProgramCoordinatorList.html')
+
+def ProgramList(request):
+    return render(request, 'ProgramList.html')
