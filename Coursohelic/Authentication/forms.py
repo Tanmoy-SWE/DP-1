f6b5c1ee9930f4202a2d65dd75608447a8f133e8
Sadik Yasin Eftee <79369612+Sadik-Yasin-Eftee@users.noreply.github.com>
2022-10-15 18:29:44 +0600

    Fixed the UI of admin registration

--- Coursohelic/Authentication/forms.py
+++ Coursohelic/Authentication/forms.py
@@ -1,10 +1,49 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+
 from .models import User
 
+
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required') 
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "p-2 mt-8 rounded-xl",
+        "type": "text",
+        "placeholder": "Username"
+    }))
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "p-2 mt-1 rounded-xl",
+        "type": "text",
+        "placeholder": "Full name"
+    }))
+
+    institution = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "p-2 mt-1 rounded-xl",
+        "type": "text",
+        "placeholder": "Institution"
+    }))
+
+    email = forms.EmailField(widget=forms.TextInput(attrs={
+        "class": "p-2 mt-1 rounded-xl",
+        "type": "email",
+        "placeholder": "Email"
+    }))
+
+    password1 = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "p-2 mt-1 rounded-xl",
+        "type": "password",
+        "placeholder": "Password"
+    }))
+
+    password2 = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "p-2 mt-1 rounded-xl",
+        "type": "password",
+        "placeholder": "Confirm Password"
+    }))
+
     class Meta:
-        model= User
-        fields = ['first_name', 'institution', 'email', 'username', 'password1', 'password2']
+        model = User
+        fields = ['first_name', 'institution', 'email',
+                  'username', 'password1', 'password2']
