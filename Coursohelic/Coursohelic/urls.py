f5994e6f8b35fa8f755651a7ce25bd28eb476a64
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-10-17 16:20:29 +0600

    Log Out implemented

--- Coursohelic/Coursohelic/urls.py
+++ Coursohelic/Coursohelic/urls.py
@@ -23,8 +23,8 @@ from django.contrib.auth.views import LogoutView,LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Home.urls')),
-    path('institutionAdmin/',include('InstitutionAdmin.urls')),
+    path('auth/',include('Authentication.urls')),
+    path('auth/institutionAdmin/',include('InstitutionAdmin.urls')),
     path('instructor/',include('Instructor.urls')),
     path('coordinator/',include('Coordinator.urls')),
-    path('auth/',include('Authentication.urls'))
 ]
