8816dcf8ee6fa797b328a0aee3a4f72b85d41702
Lamia45711704 <priankamaheru@iut-dhaka.edu>
2022-11-08 00:52:05 +0600

    Revert "Merging PDF added"
    This reverts commit 523844fafe46f04d1f34a3c407b0a1fd333706d1.

--- Coursohelic/Instructor/views.py
+++ Coursohelic/Instructor/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from Coordinator.models import AssignedCourses
-from PyPDF2 import PdfFileMerger
 
 # Create your views here.
 def login(request):
@@ -33,21 +32,3 @@ def course_list(request):
 #    print(instrList)
 #    context = {'instructors': instrList}
 #    return render(request, 'Program Coordinator/InstructorList.html', context)
-
-
-
-def Merge_pdf():
-    #Create and instance of PdfFileMerger() class
-    merger = PdfFileMerger()
-    #Create a list with file names
-    files = ['file1.pdf','file2.pdf','pdf3.pdf']
-    pdf_files = []
-    for i in range(0, len(files)):
-        pdf_files.append('pdf_files/'+files[i])
-    #Iterate over the list of file names
-    for pdf_file in pdf_files:
-        #Append PDF files
-        merger.append(pdf_file)
-    #Write out the merged PDF
-    merger.write("merged_3_pages.pdf")
-    merger.close()
