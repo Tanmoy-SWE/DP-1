2eec235bdd8363eac2c3402adc71b6ceb453fb4f
mamanyusufkhan <mamanyusufkhan@gmail.com>
2022-11-09 23:59:15 +0600

    Updated Course Outcome Schema

--- Coursohelic/Instructor/models.py
+++ Coursohelic/Instructor/models.py
@@ -5,6 +5,7 @@ from Coordinator.models import AssignedCourses, Program_Outcome
 class Course_Outcome(models.Model):
     c_code = models.CharField(max_length = 200, default=None)
     description = models.TextField(max_length = 300, default=None)
+    total_marks = models.DecimalField(max_digits = 6, decimal_places = 2, default=0)
     course_assigned = models.ForeignKey(AssignedCourses, on_delete = models.CASCADE)
 
 
