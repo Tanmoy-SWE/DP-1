551d4c79d6883d555bda92444fcc94b6cdb8bfd5
mamanyusufkhan <mamanyusufkhan@gmail.com>
2022-11-09 23:28:13 +0600

    Made Course Outcome and Mapping Schema

--- Coursohelic/Instructor/models.py
+++ Coursohelic/Instructor/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from Coordinator.models import AssignedCourses, Program_Outcome
 
 # Create your models here.
+class Course_Outcome(models.Model):
+    c_code = models.CharField(max_length = 200, default=None)
+    description = models.TextField(max_length = 300, default=None)
+    course_assigned = models.ForeignKey(AssignedCourses, on_delete = models.CASCADE)
+
+
+class Mapping(models.Model):
+    program_outcome = models.ForeignKey(Program_Outcome, on_delete = models.CASCADE)
+    course_outcome = models.ForeignKey(Course_Outcome, on_delete = models.CASCADE)
+    weight = models.DecimalField(max_digits = 5, decimal_places = 2, default=0)
+    course_assigned = models.ForeignKey(AssignedCourses, on_delete = models.CASCADE)
+
+
