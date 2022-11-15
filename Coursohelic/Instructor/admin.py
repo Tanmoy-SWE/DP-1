c145777939fedc0ed81e7c6b7da39075fb63e4b4
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2022-11-15 21:05:09 +0600

    Reverted to Previous DB

--- Coursohelic/Instructor/admin.py
+++ Coursohelic/Instructor/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Mapping, Course_Outcome, PdfFiles, Questions, Students
+from .models import Mapping, Course_Outcome, PdfFiles
 
 # Register your models here.
 admin.site.register(Mapping)
 admin.site.register(Course_Outcome)
 admin.site.register(PdfFiles)
-admin.site.register(Questions)
-admin.site.register(Students)
--- Coursohelic/Instructor/models.py
+++ Coursohelic/Instructor/models.py
@@ -1,39 +1,18 @@
 from django.db import models
-from Coordinator.models import AssignedCourses, Program_Outcome, Course
+from Coordinator.models import AssignedCourses, Program_Outcome
 
 # Create your models here.
 class Course_Outcome(models.Model):
-    c_code = models.ForeignKey(Course, on_delete=models.CASCADE),
-    co_id = models.CharField(max_length=20, default="CO1")
+    c_code = models.CharField(max_length = 200, default=None)
     description = models.TextField(max_length = 300, default=None)
     total_marks = models.DecimalField(max_digits = 6, decimal_places = 2, default=0)
-
-
-class Mapping(models.Model):
-    program_outcome = models.ForeignKey(Program_Outcome, on_delete = models.CASCADE)
-    co_id = models.ForeignKey(Course_Outcome, on_delete = models.CASCADE)
-    weight = models.DecimalField(max_digits = 5, decimal_places = 2, default=0)
     course_assigned = models.ForeignKey(AssignedCourses, on_delete = models.CASCADE)
+class Mapping(models.Model):
+     program_outcome = models.ForeignKey(Program_Outcome, on_delete = models.CASCADE)
+     course_outcome = models.ForeignKey(Course_Outcome, on_delete = models.CASCADE)
+     weight = models.DecimalField(max_digits = 5, decimal_places = 2, default=0)
+     course_assigned = models.ForeignKey(AssignedCourses, on_delete = models.CASCADE)
     
 class PdfFiles(models.Model):
     name = models.CharField(max_length=500, default=None)
     filepath= models.FileField(upload_to='files/', null=True, verbose_name="")
-    c_code = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True)
-    
-class Questions(models.Model):
-    co_id = models.ForeignKey(Course_Outcome, on_delete = models.CASCADE)
-    question_no = models.IntegerField(default=0)
-    total_marks = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-    exam_type = models.CharField(max_length=100, default=None)
-
-class Students(models.Model):
-    student_id = models.CharField(max_length=20, default=None, primary_key = True)
-    c_code = models.ForeignKey(AssignedCourses, on_delete= models.CASCADE)
-    name = models.CharField(max_length=300, default=None)
-
-class Marking(models.Model):
-    student = models.ForeignKey(Students, on_delete = models.CASCADE)
-    question = models.ForeignKey(Questions, on_delete = models.CASCADE)
-    marks_obtained = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-
-
